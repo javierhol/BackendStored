5ce098bc8017fa47e66826f8abbad24eeca9be8d
danielsansp <daniel1008ospina@gmail.com>
2022-10-20 09:32:15 -0500

    server sucessfull

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,27 @@
 import express from "express";
-import path from "path";
+import path from "path"
 import {PORT} from "../config/config"
 const AppServer: express.Application = express();
 
+console.log("Hola");
+
 
 const startServer = () => {
-
-    AppServer.use(express.static(path.join(__dirname, "views")))
-
-
+    try {
+        const port: Number = 8080;
+        AppServer.use( express.static( path.join( __dirname, "public" ) ) );
+        AppServer.use( express.json() );
+        AppServer.use( express.urlencoded( { extended: true } ) )
+    
+        AppServer.listen( PORT || port, () => {
+        
+            console.log( "Estas conectado en el puerto:", PORT );
+        
+        } )
+    } catch ( error:any ) {
+        
+       throw new Error( error);
+        
+    }
 }
+startServer()
