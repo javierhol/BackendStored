613491d8691b453e64decaf5491096717d653ffc
Angie Mejia <mejiagonzaleza881@gmail.com>
2022-10-21 11:47:12 -0500

    server find

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,21 +1,25 @@
 import express from "express";
 import path from "path"
 import {PORT} from "../config/config"
-const AppServer: express.Application = express();
-
-console.log("Hola");
-
-
+import cors from "cors"
+const AppServer: express.Application = express();console.log("Hola");
 const startServer = () => {
     try {
+        const urlConnectionAcceso:string = "http://localhost:3001"
+        const statusCors:number = 200;
         const port: Number = 8080;
+        AppServer.use(cors({
+            origin: [urlConnectionAcceso, 'https://www.google.com/*'],
+            methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+            optionsSuccessStatus:statusCors
+        }));
         AppServer.use( express.static( path.join( __dirname, "public" ) ) );
         AppServer.use( express.json() );
         AppServer.use( express.urlencoded( { extended: true } ) )
     
         AppServer.listen( PORT || port, () => {
         
-            console.log( "Estas conectado en el puerto:", PORT );
+            console.log( "connection in the port: :", PORT );
         
         } )
     } catch ( error:any ) {
